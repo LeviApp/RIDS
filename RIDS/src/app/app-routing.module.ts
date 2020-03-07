cd849f50df1d493b79152eefc66127f474b62cef
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-03-07 00:23:14 -0700

    start of angular switch-over

--- RIDS/src/app/app-routing.module.ts
+++ RIDS/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component'
+import { PlaceComponent } from './place/place.component'
+import { SuspectComponent } from './suspect/suspect.component'
+import { WeaponComponent } from './weapon/weapon.component'
 
+const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'place-info', component: PlaceComponent },
+  {path: 'suspect-info', component: SuspectComponent },
+  {path: 'weapon-info', component: WeaponComponent },
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
