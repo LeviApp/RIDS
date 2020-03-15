5290555ed1259ec0baf2c08c72b17f38a6e1ae5f
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-03-15 16:39:02 -0600

    Angular Suspect Page Added

--- RIDS/src/app/suspect/suspect.component.ts
+++ RIDS/src/app/suspect/suspect.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewChildren, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-suspect',
@@ -7,9 +7,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuspectComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
   }
 
+  @ViewChildren('tab') tabs;
+  @ViewChildren('section') sections;
+
+classes = ['Gender','Race','Height','Weight', 'Age', 'Hair', 'Face', 'Unique'];
+index = 0;
+
+showTab(value) {
+
+  for (let val of this.tabs) {
+    if (val.nativeElement.innerHTML === value) {
+      this.renderer.addClass(val.nativeElement,'clicked')
+      console.log(val.nativeElement)
+    }
+
+    else {
+      this.renderer.removeClass(val.nativeElement,'clicked')
+    }
+  }
+
+  for (let val of this.sections) {
+    if (val.nativeElement.textContent.search(value) === 0) {
+      this.renderer.removeClass(val.nativeElement,'invisible')
+      this.renderer.addClass(val.nativeElement,'visible')
+      console.log(val.nativeElement)
+    }
+
+    else {
+      this.renderer.removeClass(val.nativeElement,'visible')
+      this.renderer.addClass(val.nativeElement,'invisible')
+
+    }
+  }
+
+
 }
+
+showTabArrow(arrow) {
+  if (arrow === 'right') {
+    if (this.index === this.classes.length-1) {
+      this.index = 0
+    }
+
+    else {
+      this.index++;
+    }
+  }
+
+  else {
+    if (this.index === 0) {
+      this.index = this.classes.length-1
+    }
+
+    else {
+      this.index--;
+    }
+  }
+
+  for (let val of this.sections) {
+    if (val.nativeElement.textContent.search(this.classes[this.index]) === 0) {
+      this.renderer.removeClass(val.nativeElement,'invisible')
+      this.renderer.addClass(val.nativeElement,'visible')
+      console.log(val.nativeElement)
+    }
+
+    else {
+      this.renderer.removeClass(val.nativeElement,'visible')
+      this.renderer.addClass(val.nativeElement,'invisible')
+    }
+  }
+}
+}
+
