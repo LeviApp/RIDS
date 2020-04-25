c7fbd8e077f3b23befbf0451811ae86f13a5d1eb
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-04-24 22:32:06 -0600

    Initial p5 setup

--- RIDS/src/app/game/game.component.ts
+++ RIDS/src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {GameService} from '../game.service'
+import {GameService} from '../game.service';
+import * as p5 from 'p5';
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
@@ -7,12 +9,36 @@ import {GameService} from '../game.service'
 })
 export class GameComponent implements OnInit {
   public cities = [];
+  private p5;
   constructor(private _gameService: GameService ) { }
 
   ngOnInit() {
   
     this._gameService.getCities().subscribe(data => this.cities = data)
     console.log(this.cities)
-  }
+    const sketch = (s) => {
+      let x = 1;
+      s.preload = () => {
+        // preload code
+      }
 
+      s.setup = () => {
+        s.createCanvas(400, 400);
+      };
+
+      s.draw = () => {
+        s.background(255);
+        s.rect(x, x, x, x);
+        // if (x<100) {x=x+1}
+        // else {x=1}
+      };
+    }
+
+    let canvas = new p5(sketch);
+
+  }
 }
+
+
+
+
