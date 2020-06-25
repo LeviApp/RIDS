bb35c302c963288c5ead817265aba90cbafbe9ee
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2020-06-25 17:52:17 -0600

    more game functionality

--- RIDS/src/app/map/map.component.ts
+++ RIDS/src/app/map/map.component.ts
@@ -28,23 +28,44 @@ export class MapComponent implements OnInit {
     console.log(this.profileJson, 'this is it')
     const sketch = (s) => {
       let x = 50;
+      let btns;
+      let characterOptions;
+      let proof;
+
       s.preload = () => {
         // preload code
+      
       }
 
       s.setup = () => {
         let map = s.createCanvas(400, 400);
-        map.parent('field');
+        map.parent('char-contain');
+        btns = s.selectAll('.btn')
+        characterOptions = s.select('#field')
+        proof = s.select('#pro')
 
+        map.hide()
+
+        for (let i = 0;i<btns.length;i++) {
+          btns[i].mousePressed(() => {
+            characterOptions.hide();
+            proof.hide();
+            map.show();
+
+
+          })
+
+        }
       };
 
-
+   
 
       s.draw = () => {
         s.background(0);
         s.rect(x, x, x, x);
-        // if (x<100) {x=x+1}
-        // else {x=1}
+        if (x<100) {x=x+1}
+        else {x=1}
+        console.log(characterOptions)
       };
       
     }
