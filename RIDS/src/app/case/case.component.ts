a01016d0192ead8caeebd3cf82c56ebb6e3bac39
Levi Appenfelder <levij.app@gmail.com>
2025-08-12 22:52:41 -0600

    case details form draft implemented

--- RIDS/src/app/case/case.component.ts
+++ RIDS/src/app/case/case.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { GameService } from '../game.service'
 import {FormControl, FormGroup, FormsModule, Validators} from '@angular/forms';
-
+import { caseOptions } from '../game'
 @Component({
   selector: 'app-case',
   templateUrl: './case.component.html',
@@ -17,24 +17,26 @@ export class CaseComponent implements OnInit, AfterViewInit {
 
     caseFile: any = {};
 
-    caseForm: FormGroup
+    caseForm: FormGroup;
+
+    caseOptions: any = caseOptions;
 
 
 
   ngOnInit(): void {
     this.caseForm = new FormGroup({
-      'name': new FormControl('', [Validators.required]),
-      'gender': new FormControl('', [Validators.required]),
-      'race': new FormControl('', [Validators.required]),
-      'height': new FormControl('', [Validators.required]),
-      'age': new FormControl('', [Validators.required]),
-      'weight': new FormControl('', [Validators.required]),
-      'hair_color': new FormControl('', [Validators.required]),
-      'face_feature': new FormControl('', [Validators.required]),
-      'unique_feature': new FormControl('', [Validators.required]),
-      'crime_place': new FormControl('', [Validators.required]),
-      'weapon': new FormControl('', [Validators.required]),
-      'notebook': new FormControl('', [Validators.required]),
+      'name': new FormControl('--------', [Validators.required]),
+      'gender': new FormControl('--------', [Validators.required]),
+      'race': new FormControl('--------', [Validators.required]),
+      'height': new FormControl('--------', [Validators.required]),
+      'age': new FormControl('--------', [Validators.required]),
+      'weight': new FormControl('--------', [Validators.required]),
+      'hair_color': new FormControl('--------', [Validators.required]),
+      'face_feature': new FormControl('--------', [Validators.required]),
+      'unique_feature': new FormControl('--------', [Validators.required]),
+      'crime_place': new FormControl('--------', [Validators.required]),
+      'weapon': new FormControl('--------', [Validators.required]),
+      'notebook': new FormControl('--------', [Validators.required]),
     });
   }
 
@@ -46,6 +48,15 @@ export class CaseComponent implements OnInit, AfterViewInit {
     this._gameService.addCase(userId, 0).subscribe((data) => {
       console.log("case added!", {data})
     })
+  }
+
+  saveCaseDetails() {
+        // Check if the form is valid before logging
+        if (this.caseForm.valid) {
+          console.log("CASE FILE:", this.caseForm.value);
+        } else {
+          console.log('Form is invalid.');
+        }
   }
 
 
